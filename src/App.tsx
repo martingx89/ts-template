2ab40383dd264852cbe6a49a3367e2926c5627bc
Marcin Adamiak <59664314+martingx89@users.noreply.github.com>
2023-12-27 08:48:34 +0000

    ADD: shortID generator

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { Message } from './types';
-import { useAppSelector, useAppDispatch } from './store/store';
-import { AddMessageActon } from './store/actions';
-import { selectMessages } from './store/selectors';
+import React from "react";
+import "./App.css";
+import { Message } from "./types";
+import { useAppSelector, useAppDispatch } from "./store/store";
+import { AddMessageActon, DeleteMessageAction, UpdateMessageAction } from "./store/actions";
+import { selectMessages } from "./store/selectors";
+import shortid from 'shortid';
 
-function App() {
+const App = () => {
   const { messages } = useAppSelector(selectMessages);
 
   const dispatch = useAppDispatch();
@@ -15,33 +15,34 @@ function App() {
     dispatch(AddMessageActon(message));
   };
 
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
-          Learn React
-        </a>
-        <button
-          onClick={() =>
-            addMessage({
-              id: 'm1',
-              level: 'info',
-              text: 'info',
-            })
-          }>
-          Add
-        </button>
+  const deleteMessage = (message: Message) => {
+    dispatch(DeleteMessageAction(message))
+  }
 
-        {messages.map((el) => (
+  const updateMessage = (message: Message) => {
+    dispatch(UpdateMessageAction(message))
+  }
+
+  return (
+    <div className="App">
+      <button
+        onClick={() =>
+          addMessage({
+            id: shortid.generate(),
+            level: "info",
+            text: "info",
+          })
+        }
+      >
+        Add
+      </button>
+      {messages.map((el) => (
+        <div key={el.id}>
           <p>{el.text}</p>
-        ))}
-      </header>
+        </div>
+      ))}
     </div>
   );
-}
+};
 
 export default App;
